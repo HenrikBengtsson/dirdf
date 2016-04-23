a4443f76cb7915d55543ae6b1cd3af3b55697dbb
hb <hb@aroma-project.org>
2016-04-23 06:13:52 -0700

    BUG FIX: dirdf(..., template) was broken

--- R/dirdf.R
+++ R/dirdf.R
@@ -26,14 +26,8 @@
 #'
 #' @export
 dirdf <- function(paths, template=NULL, regexp=NULL, colnames=NULL, missing=NA_character_, recursive=TRUE, ...) {
-  if (!is.null(template)) {
-    res <- templateToRegex(template)
-    regexp <- res$pattern
-    colnames <- res$names
-  }
-
   pathnames <- lapply(paths, FUN=dir, recursive=recursive, ...)
   pathnames <- unlist(pathnames, use.names=FALSE)
 
-  dirdf_parse(pathnames, colnames=colnames, regexp=regexp, missing=missing)
+  dirdf_parse(pathnames, template=template, colnames=colnames, regexp=regexp, missing=missing)
 }
